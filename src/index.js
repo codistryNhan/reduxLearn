41b4daa8e476e64544d02380235dfaa573a6b8c8
Nhan <nhan@codistry.io>
2018-04-19 00:19:31 -0700

    Sent data to UI

--- src/index.js
+++ src/index.js
@@ -3,11 +3,20 @@ import { createPost, editPost, setFilter } from './actions';
 import appReducer from './reducers';
 
 let store = createStore(appReducer);
+const root = document.getElementById('root');
 
-const unsubscribe = store.subscribe(() => {
-  console.log('state changed:', store.getState());
-})
+const render = () => {
+  root.innerHTML = '';
+  const { posts } = store.getState();
 
+  posts.forEach((post) => {
+    const item = document.createElement('li');
+    const text = document.createTextNode(post.user + ' - ' + post.text);
+    item.appendChild(text);
+    root.appendChild(item);
+  });
+}
+
+const stopRender = store.subscribe(render);
 store.dispatch(createPost('dan', 'hello world'));
-store.dispatch(createPost('dan', 'hello world'));
-store.dispatch(editPost(0, 'hello guys'));
+store.dispatch(createPost('des', 'second post'));
