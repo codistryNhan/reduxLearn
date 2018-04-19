b26cf36db8fa1cc25b9c69c99d2e200bbb41d02a
Nhan <nhan@codistry.io>
2018-04-19 00:07:51 -0700

    Added createStore from redux

--- src/index.js
+++ src/index.js
@@ -1,15 +1,7 @@
+import { createStore } from 'redux';
 import { createPost, editPost, setFilter } from './actions';
 import appReducer from './reducers';
 
-let state = appReducer(undefined, { type: 'INIT_ACTION'});
-console.log('initial state:', state)
+let store = createStore(appReducer);
 
-
-state = appReducer(state, createPost('dan', 'test'));
-console.log('state after createPost:', state);
-
-state = appReducer(state, editPost(0, 'edited post'));
-console.log('state after editPost:', state);
-
-state = appReducer(state, setFilter('none'));
-console.log('state after setFilter:', state);
+console.log(store.getState());
